6e95397c778a2a968ce5b459e925c21c72df06c5
Kenert Kulu <kenertkulu@gmail.com>
2022-05-15 18:47:42 +0300

    Add CSV parsing

--- src/Format/Formats/Csv.php
+++ src/Format/Formats/Csv.php
@@ -6,6 +6,30 @@ class Csv extends BaseFormat
 {
     public function formatData(): void
     {
-        // TODO: Implement formatData() method.
+        $parsedCsv = $this->getParsedCsv();
+
+        $this->setFormattedData([
+            'user_role' => $parsedCsv["roleCode"],
+            'user_level' => $parsedCsv["levelCode"],
+            'user_department' => $parsedCsv["departmentCode"],
+        ]);
+    }
+
+    private function getParsedCsv(): array
+    {
+        $csv = explode("\n", $this->rawData);
+
+        $parsedCsv = [];
+        $headers = str_getcsv($csv[0], ';');
+        unset($csv[0]);
+
+        foreach ($csv as $line) {
+            $lineArr = str_getcsv($line, ';');
+            foreach ($lineArr as $index => $item) {
+                $parsedCsv[$headers[$index]] = $item;
+            }
+        }
+
+        return $parsedCsv;
     }
 }
